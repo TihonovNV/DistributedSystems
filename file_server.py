17840923f160d691cec5ab7b113751b0de7af0c4
lolkek1337 <minetown2222@gmail.com>
2020-09-19 16:13:37 +0300

    second commit

---
+++ file_server.py
@@ -0,0 +1,53 @@
+# I used some code from this source
+# https://www.thepythoncode.com/article/send-receive-files-using-sockets-python
+# and from DS lab sample as well
+
+import socket
+import os
+from threading import Thread
+
+BUFFER_SIZE = 4096
+
+class ClientListener(Thread):
+    def __init__(self, name: str, sock: socket.socket):
+        super().__init__(daemon=True)
+        self.sock = sock
+        self.name = name
+
+    def run(self):
+        filename = self.sock.recv(BUFFER_SIZE).decode()
+        dot = filename.find('.')
+        extension = '' if dot == -1 else filename[dot + 1:]
+        name = filename if dot == -1 else filename[:dot]
+        if(os.path.isfile(filename)):
+            i = 1
+            while True:
+                if (os.path.isfile(name + "_copy" + str(i) + '.' + extension)):
+                    i += 1
+                else:
+                    filename = name + "_copy" + str(i) + '.' + extension
+                    break
+        with open(filename, "wb") as f:
+            while True:
+                data = self.sock.recv(BUFFER_SIZE)
+                if not data:
+                    break
+                f.write(data)
+        print("File received succesfully")
+        self.sock.close()
+
+def main():
+    next_name = 1
+
+    s = socket.socket()
+    s.bind(('', 8800))
+    s.listen()
+    while True:
+        con, addr = s.accept()
+        name = 'user_' + str(next_name) 
+        next_name += 1
+        print(str(addr) + ' connected as ' + name)
+        ClientListener(name, con).start()
+
+if __name__ == "__main__":
+    main()
---
+++ send_file.py
@@ -0,0 +1,43 @@
+# I used some code from this source
+# https://www.thepythoncode.com/article/send-receive-files-using-sockets-python
+# and from DS lab sample as well
+
+import socket
+import os
+import sys
+from time import sleep
+import tqdm
+
+BUFFER_SIZE = 4096
+
+def main(argv):
+  filename = argv[0]
+  host = argv[1]
+  port = int(argv[2])
+
+  try : 
+    filesize = os.path.getsize(filename) 
+  
+  except OSError : 
+    print("File '%s' does not exists or is inaccessible" %filename) 
+    sys.exit()  
+
+  sock = socket.socket()
+  print("Connecting to server...")
+  sock.connect((host, port))
+  print("Connected to server, started file sending...")
+  sock.send(f"{filename}".encode())
+  sleep(1)
+  with open(filename, "rb") as f:
+    progress = tqdm.tqdm(range(filesize), "Progress", unit="B", unit_scale=True, unit_divisor=1024)
+    while True:
+      bytes_read = f.read(BUFFER_SIZE)
+      if not bytes_read:
+        break
+      sock.sendall(bytes_read)
+      progress.update(len(bytes_read))
+  sock.close()
+
+
+if __name__=="__main__":
+  main(sys.argv[1:])
